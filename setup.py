4fd08f7ae305cd60cdb8d94ab566db52f63e7fff
ahcorde <ahcorde@gmail.com>
2020-05-14 12:18:46 +0200

    Fixed package to run with ros2 run
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 package_name = 'rqt_action'
 setup(
     name=package_name,
-    version='0.4.9',
+    version='1.0.1',
     package_dir={'': 'src'},
     packages=[package_name],
     data_files=[
@@ -20,7 +20,7 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
@@ -31,4 +31,9 @@ setup(
         'ROS core your rqt instance connects to.'
     ),
     license='BSD',
+    entry_points={
+        'console_scripts': [
+            'rqt_action = ' + package_name + '.main:main',
+        ],
+    },
 )
