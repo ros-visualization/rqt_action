739e4db4e75a5c7a860c869b304f6388c21fc3f0
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-07-24 17:03:32 +0200

    fix setuptools deprecations (#19)
    (cherry picked from commit d4063e0ce3393e15c517d82c40802cb29719462b)

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
@@ -28,7 +27,11 @@ setup(
         'rqt_action provides a feature to introspect all available ROS action types.'
     ),
     license='BSD',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'rqt_action = ' + package_name + '.main:main',
